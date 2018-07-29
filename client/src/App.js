a4397d76b701458613beac895b1a28fa2558dd65
Matúš Šťastný <stastny@freevision.sk>
2018-07-30 01:09:45 +0200

    Showed data from response in App, added submit button and method for handling submit, updated styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,59 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+
 import logo from './logo.svg';
 import './App.css';
+import Keyboard from './components/Keyboard';
 
 class App extends Component {
-    state = {
-        response: ''
-    };
     constructor(props) {
         super(props);
         this.state = {
+            response: [],
             inputValue: ''
         };
     }
 
-    componentDidMount() {
+    handleSubmit = () => {
         this.callApi()
-            .then(res => this.setState({ response: res.express }))
+            .then(res => this.setState({ response: res.words }))
             .catch(err => console.log(err));
-    }
+    };
 
     callApi = async () => {
-        const response = await fetch('/api/hello');
+        const response = await fetch(`/api/${this.state.inputValue}`);
         const body = await response.json();
 
         if (response.status !== 200) throw Error(body.message);
 
         return body;
     };
+
     handleInputChange = e => {
         this.setState({ inputValue: e.target.value });
     };
+
     handleKeyboardButtonClick = value => {
         this.setState({ inputValue: `${this.state.inputValue}${value}` });
     };
+
     render() {
+        const { inputValue, response } = this.state;
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">{this.state.response}</p>
-                <input value={inputValue} onChange={this.handleInputChange} />
+                <input className="number-input" value={inputValue} onChange={this.handleInputChange} />
+                <button onClick={this.handleSubmit}>Call API</button>
+                <div className="words-wrapper">
+                    {response.map((word, index) => (
+                        <span className={`word ${word.isRealWord ? 'word--is-real' : ''}`} key={index}>
+                            {word.text}
+                        </span>
+                    ))}
+                </div>
                 <Keyboard onButtonPress={this.handleKeyboardButtonClick} />
             </div>
         );
