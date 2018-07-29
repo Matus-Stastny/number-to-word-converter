081995cf210cbb89d364f95c09157bef7aa6b4fe
Matúš Šťastný <stastny@freevision.sk>
2018-07-29 17:17:56 +0200

    Improved keyboard UI

--- client/src/components/Keyboard.js
+++ client/src/components/Keyboard.js
@@ -5,72 +5,72 @@ const Keyboard = ({ onButtonPress }) => {
     return (
         <div className="keyboard">
             <div className="keyboard__line">
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('1')}
-                >
-                    1
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('2')}
-                >
-                    2
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('3')}
-                >
-                    3
-                </button>
+                <div className="keyboard__button" onClick={() => onButtonPress('1')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">1</span>
+                        <span className="keyboard__letters">-</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('2')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">2</span>
+                        <span className="keyboard__letters">a b c</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('3')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">3</span>
+                        <span className="keyboard__letters">d e f</span>
+                    </div>
+                </div>
             </div>
             <div className="keyboard__line">
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('4')}
-                >
-                    4
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('5')}
-                >
-                    5
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('6')}
-                >
-                    6
-                </button>
+                <div className="keyboard__button" onClick={() => onButtonPress('4')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">4</span>
+                        <span className="keyboard__letters">g h i</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('5')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">5</span>
+                        <span className="keyboard__letters">j k l</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('6')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">6</span>
+                        <span className="keyboard__letters">m n o</span>
+                    </div>
+                </div>
             </div>
             <div className="keyboard__line">
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('7')}
-                >
-                    7
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('8')}
-                >
-                    8
-                </button>
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('6')}
-                >
-                    9
-                </button>
+                <div className="keyboard__button" onClick={() => onButtonPress('7')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">7</span>
+                        <span className="keyboard__letters">p q r s</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('8')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">8</span>
+                        <span className="keyboard__letters">t u v</span>
+                    </div>
+                </div>
+                <div className="keyboard__button" onClick={() => onButtonPress('9')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">9</span>
+                        <span className="keyboard__letters">w x y z</span>
+                    </div>
+                </div>
             </div>
             <div className="keyboard__line">
-                <button
-                    className="keyboard__button"
-                    onClick={() => onButtonPress('0')}
-                >
-                    0
-                </button>
+                <div className="keyboard__button" onClick={() => onButtonPress('0')}>
+                    <div className="keyboard__chars-wrapper">
+                        <span className="keyboard__number">0</span>
+                        <span className="keyboard__letters">? ! .</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
